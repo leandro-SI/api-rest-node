9d34afe89b6d9219efb359799ade9e4af2c9268b
leandro-si <leandro.almeida@uvvnet.com.br>
2024-07-18 13:50:37 -0300

    estrutura inicial

---
+++ index.js
@@ -0,0 +1,16 @@
+var bodyParser = require('body-parser')
+var express = require("express")
+var app = express()
+var router = require("./routes/routes")
+var port = 3001
+ 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.use("/",router);
+
+app.listen(port,() => {
+    console.log(`Servidor rodando na porta ${port}`)
+});
